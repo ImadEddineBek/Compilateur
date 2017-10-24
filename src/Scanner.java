c3090307a9fc9959f64e7f5f2035316fed86c885
ImadEddineBek <bek34046629@yahoo.com>
2017-10-24 19:24:39 +0100

    first Try

---
+++ src/Scanner.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    private List<String> source;
+    private char currentChar;
+    private Token currentToken;
+
+    public Scanner(String path) {
+        try {
+            source = Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            source = new ArrayList<>();
+        }
+    }
+
+    public boolean isLetter(char c) {
+        return (c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A');
+    }
+
+    public boolean isDigit(char c) {
+        return (c <= '9' && c >= '0');
+    }
+
+    public boolean isOperator(char c) {
+        return (c == '+' || c == '-' || c == '*' || c == '/' ||
+
+                c == '=' || c == '<' || c == '>' || c == '\\' ||
+
+                c == '&' || c == '@' || c == '%' || c == '^' ||
+
+                c == '?');
+    }
+
+    public ArrayList<Token> scan() {
+        for (String line : source) {
+            char[] chars = line.toCharArray();
+            for (char aChar : chars) {
+
+            }
+        }
+    }
+}
