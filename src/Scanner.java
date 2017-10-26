6fc3b2ff69d26790429b0fb7204d54c0fc152042
ImadEddineBek <bek34046629@yahoo.com>
2017-10-26 15:11:37 +0100

    first Try

--- src/Scanner.java
+++ src/Scanner.java
@@ -9,7 +9,7 @@ public class Scanner {
     private int position;
     private List<String> source;
     private char currentChar;
-    private Token currentToken;
+    private StringBuilder currentToken;
     private int state;
 
     public Scanner(String path) {
@@ -49,9 +49,37 @@ public class Scanner {
         char c = text.charAt(position);
         switch (state) {
             case 0:
-                if (isaNotImportant(c)) state = 0;
-                return getNextToken();
+                if (isaNotImportant(c)) {
+                    state = 0;
+                    position++;
+                    return getNextToken();
+                }
+                if (isLetter(c)) {
+                    state = 1;
+                    return getNextToken();
+                }
+                if (isDigit(c)) {
+                    state = 3;
+                    return getNextToken();
+                }
+                if (isOperator(c)) {
+                    state = 4;
+                    return getNextToken();
+                }
+                if (c == '"') {
+                    state = 2;
+                    return getNextToken();
+                }
             case 1:
+                if (isDigit(c) || isLetter(c) || c == '_') {
+                    state = 1;
+                    currentToken.append(c);
+                    position++;
+                    return getNextToken();
+                } else if (isKeyWord(currentToken.toString())) return new Token(Type.mot_cle, currentToken.toString());
+                else return new Token(Type.id, currentToken.toString());
+            case 2:
+                if ()
 
         }
         return null;
@@ -59,5 +87,26 @@ public class Scanner {
 
     private boolean isaNotImportant(char c) {
         return c == ' ' || c == '\t' || c == '\n';
+    }
+
+    private boolean isKeyWord(String s) {
+        switch (s) {
+            case "begin":
+            case "const":
+            case "do":
+            case "else":
+            case "end":
+            case "if":
+            case "in":
+            case "let":
+            case "while":
+            case "then":
+            case "var":
+            case "function":
+            case "int":
+            case "string":
+                return true;
+        }
+        return false;
     }
 }
